9c74d0c6baefb5c60ae77ab6aca29939fe0bf596
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-11-21 17:54:37 +0000

    users show and index plust layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
+
+  def show
+    
+    @matching_user = User.where({ :username => params.fetch("a_user")}).at(0)
+
+    if @matching_user == nil
+      redirect_to("/404")
+    else
+      render({ :template => "user_templates/show"})
+    end
+  end
+  
   def index
+    
+    matching_users = User.all
+
+    @list_of_users = matching_users.order({:username => :asc})
+
     render({ :template => "user_templates/index"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   get("/users", { :controller => "users", :action => "index"})
+  get("/users/:a_user", { :controller => "users", :action => "show"})
 
 end
