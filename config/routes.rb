0f0896af684e2e012a4d0e52b1fcee3480daaf20
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-11-21 18:35:55 +0000

    photos details working for read
    undefined

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "index"})
 
   get("/users", { :controller => "users", :action => "index"})
   get("/users/:a_user", { :controller => "users", :action => "show"})
